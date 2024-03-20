747c098d79d5d95f2c734ac33057c011334f2c9d
Matthews Britto <luismatthews29@gmail.com>
2024-03-20 17:50:30 -0300

    ajuste

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch
+} from "react-router-dom";
+import { Header } from '../common/Header'
 
 export const Pages = () => {
   return (
-
-
-    <>Pages </>
-    
-   
+  <>
+    <Router>
+      <Header />
+      {/*<Switch>
+        {/*<Route path='/ '>
+        <About />
+        </Route> 
+        
+  </Switch> */}
+    </Router>
+  </>
   );
 }
